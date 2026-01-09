394e907e0cc1f7f031e3c56870b0ac7285159763
ke1-dotcom <ziismyname2@gmail.com>
2026-01-09 19:25:30 +0700

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "keii.github.io/oc-wiki",
+    baseUrl: "ke1-dotcom.github.io/oc-wiki",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
